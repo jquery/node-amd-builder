7d8e1e276fc30bc2e9834962fc2cb32ba2873aac
Ghislain Seguin <ghislain@gmail.com>
2011-12-31 23:18:12 -0800

    Added support for multi repo Added traceDeps route Cleaned up a bit

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-var app = require( 'express' ).createServer(),
+'use strict';
+
+var express = require( 'express'),
+    app = express.createServer(),
+    crypto = require( 'crypto' ),
     fs = require( 'fs' ),
     exec = require( 'child_process' ).exec,
     path = require( 'path' ),
-    spawn = require( 'child_process' ).spawn;
+    querystring = require( 'querystring' ),
+    regexp = require( './lib/regexp' ),
+    requirejs = require( './lib/r.js' );
 
 var port = 3000,
-    bareRepo = process.env.BARE_REPO || "../jquery-mobile",
-    dstDirBase = ( process.env.OUTPUT_DIR || "../src" ) + "/jquery-mobile.";
+    repoBaseDir = process.env.REPO_BASE_DIR,
+    workBaseDir = process.env.WORK_BASE_DIR;
 
-if ( process.env.SERVER === "PRODUCTION" ) {
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
     port = 80;
-}
+});
+
+app.use(express.bodyParser());
 
 app.get( '/', function ( req, res ) {
     res.send( "<h1 style='text-align: center; font-size: 120px;'>ZOMG JQM-BUILDER</h1>" );
 });
 
-app.get( '/fetch', function ( req, res ) {
+app.get( '/:repo/fetch', function ( req, res ) {
     exec( "git fetch",
         {
             encoding:'utf8',
             timeout:0,
-            cwd:bareRepo,
+            cwd: bareRepo,
             env:null
         },
         function ( error, stdout, stderr ) {
@@ -34,6 +46,47 @@ app.get( '/fetch', function ( req, res ) {
             }
         }
     );
+});
+
+app.post( '/post_receive', function ( req, res ) {
+    var payload = req.body.payload;
+
+    if ( payload ) {
+        try {
+            payload = JSON.parse( payload );
+
+            if ( payload.repository && payload.repository.name ) {
+                var repoDir = repoBaseDir + payload.repository.name + ".git";
+                path.exists( repoDir , function( exists ) {
+                    if ( exists ) {
+                        exec( "git fetch",
+                            {
+                                encoding:'utf8',
+                                timeout:0,
+                                cwd: repoDir,
+                                env:null
+                            },
+                            function ( error, stdout, stderr ) {
+                                if ( error !== null ) {
+                                    res.send( error, 500 );
+                                } else {
+                                    res.send( stdout );
+                                }
+                            }
+                        );
+                    } else {
+                        res( "Wrong door!", 404 );
+                    }
+                });
+            } else {
+                res( "Wrong door!", 404 );
+            }
+        } catch( e ) {
+            res.send( e, 500 );
+        }
+    } else {
+        res.send( "No Payload!", 400 );
+    }
 });
 
 app.get( '/:tag/checkout', function ( req, res ) {
@@ -59,13 +112,19 @@ app.get( '/:tag/checkout', function ( req, res ) {
     });
 });
 
-app.get( '/:tag/make', function ( req, res ) {
-    var include = ( req.param( "include" ) || "jquery.mobile" ).split( "," ).sort(),
-        optimize = req.param( "optimize" ) || "none",
-        dstDir = dstDirBase + req.params.tag + "/compiled/" + include.join( "+" ),
-        dstFile = dstDir + "/jquery.mobile" + (optimize !== "none" ? ".min" : "") + ".js";
-
-    include.push( "jquery.mobile.init" );
+app.get( '/:repo/:tag/make', function ( req, res ) {
+    var include = req.param( "include", "main" ).split( "," ).sort(),
+        exclude = req.param( "exclude", "" ).split( "," ).sort(),
+        optimize = req.param( "optimize", "none" ),
+        baseUrl = req.param( "baseUrl", "." ),
+        hasPragmas = !!req.param( "pragmas", false ),
+        pragmas = hasPragmas?querystring.stringify( JSON.parse( req.param( "pragmas" ) ), ",", "=" ).split( "," ).map( function(val) { return "pragmas." + val; } ):[],
+        hasPragmasOnSave = !!req.param( "pragmasOnSave", false ),
+        pragmasOnSave = hasPragmasOnSave?querystring.stringify( JSON.parse( req.param( "pragmasOnSave" ) ), ",", "=" ).split( "," ).map( function(val) { return "pragmasOnSave." + val; } ):[],
+        name = path.basename( req.param( "name", req.params.repo ), ".js" ),
+        wsDir   = workBaseDir + "/" + req.params.repo + "." + req.params.tag,
+        dstDir  = wsDir + "/compiled/" + include.join( "+" ),
+        dstFile = dstDir + "/" + name + (optimize !== "none" ? ".min" : "") + ".js";
 
     path.exists( dstFile, function ( exists ) {
         if ( exists ) {
@@ -73,16 +132,19 @@ app.get( '/:tag/make', function ( req, res ) {
         } else {
             exec( [ process.execPath,
                 __dirname + '/node_modules/.bin/r.js',
-                '-o baseUrl=js',
+                '-o',
+                'baseUrl=' + baseUrl,
                 'include=' + include.join( "," ),
-                'exclude=jquery,order',
+                exclude?'exclude=' + exclude.join( "," ):"",
                 'out=' + dstFile,
-                'pragmasOnSave.jqmBuildExclude=true',
-                'optimize=' + optimize ].join( " " ),
+                'optimize=' + optimize ]
+                .concat( pragmas )
+                .concat( pragmasOnSave )
+                .join( " " ),
                 {
                     encoding:'utf8',
                     timeout:0,
-                    cwd:dstDirBase + req.params.tag,
+                    cwd: wsDir,
                     env:null
                 },
                 function ( error, stdout, stderr ) {
@@ -93,6 +155,48 @@ app.get( '/:tag/make', function ( req, res ) {
                     } else {
                         res.download( dstFile, path.basename( dstFile ) );
                     }
+                }
+            );
+        }
+    });
+});
+
+app.get( '/:repo/:tag/dependencies', function ( req, res ) {
+    var wsDir   = workBaseDir + "/" + req.params.repo + "." + req.params.tag,
+        exclude = req.param( "exclude", "" ).split( "," ).sort(),
+        names = path.basename( req.param( "names", req.params.repo ), ".js" ).split( "," ).sort(),
+        baseUrl = wsDir + "/" + req.param( "baseUrl", "." ),
+        shasum = crypto.createHash( 'sha1' ),
+        filename = wsDir + "/deps-";
+
+    // Generate a sha on the sorted names
+    shasum.update( names.join( "," ) );
+    filename += shasum.digest( 'hex' ) + ".json";
+
+    path.exists( filename, function( exists ) {
+        if ( exists ){
+            res.sendfile( filename );
+        } else {
+            requirejs.tools.traceFiles( {
+                    baseUrl: baseUrl,
+                    modules: names.map( function( name ) { return { name: path.basename( name, ".js" ) } } ),
+                    optimize: "none"
+                },
+                function( deps ) {
+                    // Walk through the dep map and remove baseUrl and js extention
+                    var module, baseUrlRE = new RegExp( "^" + regexp.escapeString( baseUrl ) );
+                    for ( module in deps ) {
+                        deps[ module ].files = deps[ module ].files.map(
+                            function( file ) {
+                                return path.basename( file.replace( baseUrlRE, "" ), ".js" );
+                            })
+                    }
+                    fs.writeFile( filename, JSON.stringify( deps ),
+                        function (err) {
+                            if (err) throw err;
+                        }
+                    );
+                    res.json( deps );
                 }
             );
         }
