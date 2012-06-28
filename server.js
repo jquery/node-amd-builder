33a0ed2fb44278d37ed5d23bb16d8d745f69056e
Ghislain Seguin <ghislain@gmail.com>
2012-06-28 14:42:45 -0700

    Changed log format

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.configure('development', function(){
 });
 
 app.configure('production', function(){
-    app.use( express.logger( { format: ':remote-addr [:date] - :req[host] - :status ":method :url :http-version" - ":referrer" - ":user-agent"' } ) );
+    app.use( express.logger( { format: '[:date] - :status ":method :url" - :response-time ms' } ) );
 });
 
 app.use(express.bodyParser());
